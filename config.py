6fc6b42f0f0b3705599d0a2a0115d3a0746f5dd7
ronaksood <soodronak04@gmail.com>
2025-11-23 14:57:42 +0530

    added config.py and modified gitignore

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+API_KEY = os.getenv("API_KEY")
+SECRECT_KEY = os.getenv("SECRET_KEY")
+BASE_URL = os.getenv("BASE_URL")
+VERSION = os.getenv("VERSION")
+
+VANTAGE_API_KEY = os.getenv("VANTAGE_API_KEY")
+VANTAGE_API_KEY2 = os.getenv("VANTAGE_API_KEY2")
+
+DB_FILE = os.getenv("DB_FILE")
+
+FINNHUB_KEY = os.getenv("FINNHUB_KEY")
